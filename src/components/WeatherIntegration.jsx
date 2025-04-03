9663c2e9857b547f028c0a924ddc991a0d0bbf70
Zaki Dawrey <zdawrey@gmail.com>
2025-04-03 13:17:31 +0530

    Utitilies to src

--- src/components/WeatherIntegration.jsx
+++ src/components/WeatherIntegration.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Button, CircularProgress, Box, Tooltip } from "@mui/material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
-import { getCurrentLocationWeather } from "../../utilities/weatherService";
-// import { weatherService } from "@/utilities/weatherService";
+// import { getCurrentLocationWeather } from "../../utilities/weatherService";
+import { getCurrentLocationWeather } from "@/utilities/weatherService";
 import { updateRoomParameters } from "../store/store";
 
 const WeatherIntegration = ({
